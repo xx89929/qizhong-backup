405e1b21b6483ec322d821ebbfd591d08eff8059
xx89929 <543060074@qq.com>
2017-09-22 14:42:56 +0800

    add localtion_icon

--- resources/views/home/layouts/qq-kefu.blade.php
+++ resources/views/home/layouts/qq-kefu.blade.php
@@ -1,9 +1,16 @@
 <div class="fudong-div">
-    <ul class="list-unstyled">
+    <ul class="list-unstyled kefu-ul">
         <li>
             <a class="pull-right"  target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=3385434967&site=qq&menu=yes">
                 <img class="kefu-hide" src="{{url('image/ll04.png')}}">
                 <img class="kefu-show" src="{{url('image/l04.png')}}">
+            </a>
+        </li>
+
+        <li>
+            <a class="pull-right" href="#">
+                <img class="kefu-hide" src="{{url('image/location_width_icon.png')}}">
+                <img class="kefu-show" src="{{url('image/location_icon.png')}}">
             </a>
         </li>
 
